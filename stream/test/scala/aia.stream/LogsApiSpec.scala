a8a30d01b2fc175c7cb5c8980501d33f6f7efa49
umeneri <koishimst@gmail.com>
2019-09-29 10:26:30 +0900

    add log post test

--- stream/test/scala/aia.stream/LogsApiSpec.scala
+++ stream/test/scala/aia.stream/LogsApiSpec.scala
@@ -16,11 +16,28 @@ class LogsApiSpec extends WordSpec
   with MockitoSugar
   with ArgumentMatchersSugar {
 
-  val logsApi = new LogsApi(Paths.get("test/resources"), 100)
-  lazy val routes: Route = logsApi.getRoute
-
   "LogsApi getRoute" should {
+    "post entity" in {
+      val logsApi = new LogsApi(Paths.get("test/tmp"), 100)
+      lazy val routes: Route = logsApi.postRoute
+      val entity =
+        """my-host-1  | web-app | ok       | 2015-08-12T12:12:00.127Z | 5 tickets sold to RHCP.|
+          |my-host-2  | web-app | ok       | 2015-08-12T12:12:01.127Z | 3 tickets sold to RHCP.|
+          |my-host-3  | web-app | ok       | 2015-08-12T12:12:02.127Z | 1 tickets sold to RHCP.|
+          |my-host-3  | web-app | error    | 2015-08-12T12:12:03.127Z | exception occurred...|
+          |my-host-4  | web-app | error    | 2015-08-12T12:12:03.127Z | exception occurred...|
+          |
+          |""".stripMargin
+      val request = Post("/logs/1").withEntity(entity)
+
+      request ~> routes ~> check {
+        status should ===(StatusCodes.OK)
+        entityAs[String] should ===("""{"logId":"1","written":637}""")
+      }
+    }
     "get entity" in {
+      val logsApi = new LogsApi(Paths.get("test/resources"), 100)
+      lazy val routes: Route = logsApi.getRoute
       val request = Get("/logs/1")
 
       val expected =
